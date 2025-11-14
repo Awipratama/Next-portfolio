0f76c4906d4e379ac189d270f5f597d39af5accf
Awipratama <awipratama05122006@gmail.com>
2025-11-14 16:04:11 +0800

    design navbar done

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex main-page min-h-screen items-center justify-center font-sans">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
+    <div className="flex main-page w-full min-h-screen items-center justify-center font-sans">
+      <main className="flex max-w-[1150px] min-h-screen w-full flex-col items-center justify-between py-32 sm:items-start">
         <Image
           className="dark:invert"
           src="/next.svg"
--- layout/MainNavbar.tsx
+++ layout/MainNavbar.tsx
@@ -2,18 +2,18 @@
 
 export default function MainNavbar() {
   return (
-    <div className="main-navbar fixed w-full">
-      <nav className="w-full mx-auto max-w-[1250px] flex justify-between">
-        <div className="logo">
+    <div className="main-navbar fixed flex justify-center w-full">
+      <nav className="w-full max-w-[1150px] flex justify-between">
+        <div className="logo py-2">
           <img
             className="w-[100px]"
             src="/Logos/logo-white-remove-bg.png"
             alt=""
           />
         </div>
-        <ul className="flex justify-center gap-10 w-full items-center">
+        <ul className="flex justify-center gap-4 w-full items-center">
           <li>
-            <a className="nav-fonts" href="#">Home</a>
+            <a className="nav-fonts active" href="#">Home</a>
           </li>
           <li>
             <a className="nav-fonts" href="#">About Me</a>
@@ -25,8 +25,8 @@ export default function MainNavbar() {
             <a className="nav-fonts" href="#">Contact Me</a>
           </li>
         </ul>
-        <div className="cv-btn flex">
-          <button className="w-full">Get My CV</button>
+        <div className="cv-btn flex py-7">
+          <button className="cursor-pointer font-medium w-[120px] rounded-2xl bg-[#6366F1] hover:bg-[#4F46E5] px-4">Get My CV</button>
         </div>
       </nav>
     </div>

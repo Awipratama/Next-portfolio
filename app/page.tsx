edbed25d9332055c95ed238ef64efd5073de83b0
Awipratama <awipratama05122006@gmail.com>
2025-11-15 22:07:50 +0800

    gabut

--- app/page.tsx
+++ app/page.tsx
@@ -6,18 +6,27 @@ export default function Home() {
       <main className="flex max-w-[1150px] min-h-screen w-full items-center justify-between py-32">
         <div className="text-section flex flex-col w-1/2 pr-10">
           <h1 className="text-[48px]/14 font-bold mb-6">
-            Hai, I'm Awipratama <br /> an FrontEnd Web Developer
+            Hi, I'm Awipratama <br /> an FrontEnd Web Developer
           </h1>
-          <p className="pb-4">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque
-            incidunt quia explicabo optio nostrum veritatis natus fugiat maxime
-            tenetur libero.
+          <p className="pb-8">
+            I'm a passionate Front-End Developer with a strong interest in
+            creating interactive and user-friendly web experiences.
           </p>
-          <a className="text-center text-[18px] py-3 bg-[#4F46E5] hover:bg-[#6366F1] w-50 rounded-2xl" href="#">Learn More</a>
+          <a
+            className="text-center text-[18px] py-3 bg-[#4F46E5] hover:bg-[#6366F1] w-50 rounded-2xl"
+            href="#"
+          >
+            Learn More
+          </a>
         </div>
-        <div className="image-section w-1/2">
-          <img className="rounded-full w-[80%] ml-auto" src="/Images/profile-1.jpg" alt="" />
+        <div className="w-1/2">
+          <img
+            className="image-section rounded-full w-[80%] ml-auto"
+            src="/Images/profile-1.jpg"
+            alt=""
+          />
         </div>
+        <div className="shape"></div>
       </main>
     </div>
   );

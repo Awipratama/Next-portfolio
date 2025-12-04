97419b43505027179782a504d4a4d63184f8b729
Awipratama <awipratama05122006@gmail.com>
2025-12-04 18:48:28 +0800

    another update

--- layout/About.tsx
+++ layout/About.tsx
@@ -2,7 +2,10 @@ import FloatingShapes2 from "@/components/FloatingShapes2";
 
 export default function About() {
   return (
-    <section id="about" className="about-section flex w-full min-h-screen items-center justify-center font-sans py-32 bg-[#0c1b3c]">
+    <section
+      id="about"
+      className="about-section flex w-full min-h-screen items-center justify-center font-sans py-32 bg-[#0c1b3c]"
+    >
       <div className="about-container max-w-[1150px] gap-10 flex my-10">
         <FloatingShapes2 />
         <div className="about-image relative flex items-center justify-center">
@@ -22,16 +25,23 @@ export default function About() {
             professionally as a Web Developer at a private company in Bali.
           </p>
           <div className="about-card-1 flex justify-between items-center rounded-2xl p-6 bg-[#202b50] mb-4 transition-all duration-300 cursor-default hover:scale-105">
-            <h5 className="text-lg font-semibold">
-              5+ Complete Web Projects
-            </h5>
-            <a href="#" className="hover:underline">See More</a>
+            <h5 className="text-lg font-semibold">5+ Complete Web Projects</h5>
+            <a href="#" className="hover:underline">
+              See More
+            </a>
           </div>
           <div className="about-card-2 flex justify-between items-center rounded-2xl p-6 bg-[#1a2340] transition-all duration-300 cursor-default hover:scale-105">
             <h5 className="text-lg font-semibold">20+ GitHub Repository</h5>
-            <a href="#" className="hover:underline">See More</a>
+            <a href="#" className="hover:underline">
+              See More
+            </a>
           </div>
-          <a href="#" className="text-lg rounded-2xl px-6 py-4 mt-6 w-60 bg-[#4F46E5] hover:bg-[#6366F1]">Download Resume <strong>&rarr;</strong></a>
+          <a
+            href="#"
+            className="text-lg rounded-2xl px-6 py-4 mt-6 w-60 bg-[#4F46E5] hover:bg-[#6366F1]"
+          >
+            Download Resume <strong>&rarr;</strong>
+          </a>
         </div>
       </div>
     </section>
--- layout/Services.tsx
+++ layout/Services.tsx
@@ -1,10 +1,13 @@
 import { FaComputer, FaScrewdriverWrench } from "react-icons/fa6";
 import { MdDesignServices } from "react-icons/md";
 
+import FloatingShapes3 from "@/components/FloatingShapes3";
+
 export default function Services() {
   return (
-    <section className="services-section bg-[#0c1b3c] flex justify-center w-full">
-      <div className="service-container flex flex-col max-w-[1150px] my-10">
+    <section className="services-section bg-[#0c1b3c] flex justify-center w-full py-32">
+      <FloatingShapes3 />
+      <div className="service-container flex flex-col max-w-[1150px]">
         <h2 className="text-md font-bold text-center uppercase mt-16">
           Services
         </h2>
